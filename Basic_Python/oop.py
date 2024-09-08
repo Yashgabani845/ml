0f0f1c1df7a5d63f18878b54961edd53e57dd8d0
Yashgabani845 <gabaniyash845@gmail.com>
2024-09-08 21:47:59 +0530

    oop added

---
+++ Basic_Python/oop.py
@@ -0,0 +1,22 @@
+class Employee:
+    name="yash"
+    age=19
+    salary=50000
+    def getinfo(self):
+        return(f"name {self.name} age {self.age} salary{self.salary}")
+    @staticmethod
+    def getstaticinfo():
+        return ("hello")
+    #  return(f"name {name} age {age} salary{salary}")//can not excute name 
+yash= Employee()
+print(yash.getinfo())
+
+print(yash.name,yash.age,yash.salary)
+
+
+# in oop of python it passes the self if we call yash.getinfo then it will do like Employee.getinfo(yash)
+
+
+#-----------------static method---------------- then no need of self
+print(yash.getstaticinfo())
+# Unlike instance and class methods, static methods cannot access class attributes or instance attributes. In Python, we create a static method using a decorator. We write @staticmethod right above the method we want to decorate. Static methods have no implicit first argument.
